dbfad2394c0453f01b481d705d408034a4cb0e66
Alexsandro Trindade <sandrojsd@gmail.com>
2020-05-05 16:32:23 -0400

    Estilização da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home1</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x"
+          alt="Tênis"
+        />
+        <stron>Tênis muito legal</stron>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
